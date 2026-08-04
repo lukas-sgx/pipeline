5cbe698cbc60683c00d654d514e54d19d2e6e582
Lukas SOIGNEUX <lukas.soigneux@epitech.eu>
2026-08-04 16:13:35 +0200

    chore: update branch url

--- src/variables.rs
+++ src/variables.rs
@@ -1,2 +1,2 @@
-pub const MAP_XML_PATH: &'static str = "https://github.com/lukas-sgx/pipeline-gen/blob/develop/templates/map.xml?raw=true";
-pub const STABLE_URL: &'static str = "https://github.com/lukas-sgx/pipeline-gen/blob/develop/templates/";
+pub const MAP_XML_PATH: &'static str = "https://github.com/lukas-sgx/pipeline-gen/blob/stable/templates/map.xml?raw=true";
+pub const STABLE_URL: &'static str = "https://github.com/lukas-sgx/pipeline-gen/blob/stable/templates/";
