52f6f9faa69b730c8fbcd6bbc93a485414985789
Lukas SOIGNEUX <lukas.soigneux@epitech.eu>
2026-08-05 09:35:38 +0200

    chore: fmt

--- src/variables.rs
+++ src/variables.rs
@@ -1,4 +1,3 @@
 pub const MAP_XML_PATH: &str =
     "https://github.com/lukas-sgx/pipeline-gen/blob/stable/templates/map.xml?raw=true";
-pub const STABLE_URL: &str =
-    "https://github.com/lukas-sgx/pipeline-gen/blob/stable/templates/";
+pub const STABLE_URL: &str = "https://github.com/lukas-sgx/pipeline-gen/blob/stable/templates/";
